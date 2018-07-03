b77e54467708d4084c02004ed941828be26a140c
Micah Raney <micah-raney@users.noreply.github.com>
2018-07-02 21:25:02 -0400

    Better test condition switching

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ DEG = '°'
 import thermostat
 
 fake_temp_f = 70
+test_mode = 'heat'
 
 def read_temp_f():
 	return fake_temp_f
@@ -30,10 +31,13 @@ cold = 2
 heat = 3
 aux = 4
 pins = [ fan, cold, heat, aux ]
-t = thermostat.Thermostat(pins, 'cold', 'auto', 65)
+t = thermostat.Thermostat(pins, test_mode, 'auto', 65)
 
 while t.mode is not 'off' and t.get_desired_temp_f() is not read_temp_f():
-	fake_temp_f -= 1 # temp lowers
+	if test_mode == 'heat' or test_mode == 'aux':
+		fake_temp_f += 1 # temp raises
+	elif test_mode == 'cold':
+		fake_temp_f -= 1 # temp lowers
 	main_loop()
 	t.adjust_temp_f(read_temp_f())
 	print()
