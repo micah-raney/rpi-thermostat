402958fb4096841a9055087ebe4edd8d7b1d22ee
Micah Raney <micah-raney@users.noreply.github.com>
2018-07-14 21:40:24 -0400

    Add time delay to main test loop

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 DEG = '°'
 
 import thermostat
+import time
 
 fake_temp_f = 70
 test_mode = 'heat'
@@ -41,4 +42,5 @@ while t.mode is not 'off' and t.get_desired_temp_f() is not read_temp_f():
 	main_loop()
 	t.adjust_temp_f(read_temp_f())
 	print()
+	time.sleep(1)
 print("*End*\n")
