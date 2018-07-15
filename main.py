05a73845f178a3037c7f1ab73c4c9835ffef14f3
Micah Raney <micah-raney@users.noreply.github.com>
2018-07-14 20:40:14 -0400

    Minor edit

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ cold = 2
 heat = 3
 aux = 4
 pins = [ fan, cold, heat, aux ]
-t = thermostat.Thermostat(pins, test_mode, 'auto', 65)
+t = thermostat.Thermostat(pins, test_mode, 'auto', 75)
 
 while t.mode is not 'off' and t.get_desired_temp_f() is not read_temp_f():
 	if test_mode == 'heat' or test_mode == 'aux':
